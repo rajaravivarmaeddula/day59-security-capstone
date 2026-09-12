fa00f1f8ba620d9cb99981d61ed9600857a646ac
rajaravivarmaeddula <rajaravivarmaeddula@gmail.com>
2026-09-12 12:38:55 +0000

    feat: deploy master DevSecOps capstone pipeline

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Hardened Security Group
+resource "aws_security_group" "hardened_app_sg" {
+  name        = "capstone-hardened-app-sg"
+  description = "Hardened security group for capstone application"
+
+  # Allow HTTP inbound
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Restrict outbound traffic to standard web ports
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
